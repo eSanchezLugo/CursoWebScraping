7ba59a298f9795d65b89f959bf0ba864b18b4515
eSanchezLugo <alefire666@gmil.com>
2020-12-05 19:44:11 -0600

    Nuevos ejemplos

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,5 +7,10 @@ N
 nivel1_stackoverflow_scrapy.py,2\a\2a33548a055ea2c8d79e378b84f47200765a9afe
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
-@
-primerScrapy.csv,0\c\0c98db01915a424ed11571ef5a238b947a91c804
+>
+resultados.csv,3\5\350b8882238c47033d308485157576df225e3d87
+E
+nivel1_elUniversal.py,0\5\058f8e3db5ab3149209b57b285aadb5ee768a1c1
+:
+
+video.json,2\d\2d01fff4014825f54a24f4205ac1549ead66869f
---
+++ nivel1/nivel1_elUniversal.py
@@ -0,0 +1,51 @@
+from scrapy.item import Field
+from scrapy.item import  Item
+from scrapy.spiders import Spider
+from scrapy.loader import ItemLoader
+from bs4 import BeautifulSoup
+
+class Noticia(Item):
+    id = Field()
+    titular = Field()
+    descripcion = Field()
+
+class ElUniversoSpider(Spider):
+
+    name = "MiSegundoSpider"
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/71.0.3578.80 Chrome/71.0.3578.80 Safari/537.36'
+    }
+    start_urls = ['https://www.eluniverso.com/deportes']
+
+    def parse(self, response):
+        sel = Selector(response)
+        noticias = sel.xpath('//div[@class="view-content"]/div[@class="posts"]')
+        for i, elem in enumerate(noticias):
+            item = ItemLoader(Noticia(), elem)
+
+            item.add_xpath('titular', './/h2/a/text()')
+            item.add_xpath('descripcion', './/p/text()')
+            item.add_value('id', i)
+            yield item.load_item()
+
+    #def parse(self, response):
+        #soup = BeautifulSoup(response.body)
+        #contenedor_noticias = soup.find_all(class_='view-content')
+        #id = 0
+        #for contenedor in contenedor_noticias:
+            #noticias = contenedor.find_all(class_='posts', recursive = False)
+            #for noticia in noticias:
+                #item = ItemLoader(Noticia(), response.body)
+                #titular = noticia.find('h2').text.replace('\n', '').replace('\r', '')
+                #descripcion = noticia.find('p')
+                #if (descripcion):
+                    #item.add_value('descripcion', descripcion.text.replace('\n', '').replace('\r', ''))
+                #else:
+                    #item.add_value('descripcion', 'N/A')
+                    #item.add_value('titular', titular)
+                    #item.add_value('id', id)
+                    #id += 1
+                    #yield item.load_item()
+
+
+
